76cb8297f5c08c823ce152c4e02781592c03c19f
Ilias Zalyalov <Zalyalovir@gmail.com>
2020-05-09 19:04:05 +0300

    Вернул обратно

--- app/src/main/java/com/example/myapplication/MapActivity.java
+++ app/src/main/java/com/example/myapplication/MapActivity.java
@@ -19,8 +19,9 @@ public class MapActivity extends Activity {
 
         MapKitFactory.setApiKey("ec9c5eca-4299-48f6-9ab1-12aaeeb7cbfd");
 
-//        MapKitFactory.initialize(this);
+        MapKitFactory.initialize(this);
 
+        
         setContentView(R.layout.map);
         super.onCreate(savedInstanceState);
         mapView = (MapView)findViewById(R.id.mapview);
